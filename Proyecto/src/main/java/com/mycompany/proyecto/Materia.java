0a9257706c9ced515a134e1303ee2c3852b5d12d
Diego Araujo <dienarau@espol.edu.ec>
2023-07-11 21:54:52 -0500

    Modificación clase Materia y Paralelo

--- Proyecto/src/main/java/com/mycompany/proyecto/Materia.java
+++ Proyecto/src/main/java/com/mycompany/proyecto/Materia.java
@@ -1,17 +1,19 @@
 package com.mycompany.proyecto;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Materia {
     private String codigo;
     private String nombre;
     private int numeroNiveles;
-    private TerminoAcademico termino;
+    private ArrayList<TerminoAcademico> terminos;
 
-    //constructor con todos los parámetros
-    public Materia(String codigo, String nombre, int numeroNiveles, TerminoAcademico termino) {
+    public Materia(String codigo, String nombre, int numeroNiveles) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.numeroNiveles = numeroNiveles;
-        this.termino = termino;
+        terminos = new ArrayList<>();
     }
     
 
@@ -39,28 +41,100 @@ public class Materia {
         this.numeroNiveles = numeroNiveles;
     }
 
-    public TerminoAcademico getTermino() {
-        return termino;
+    public ArrayList<TerminoAcademico> getTerminos() {
+        return terminos;
     }
 
-    public void setTermino(TerminoAcademico termino) {
-        this.termino = termino;
+    public void setTermino(ArrayList<TerminoAcademico> terminos) {
+        this.terminos = terminos;
     }
     
     //método que permite ingresar una materia
-    public void ingresarMateria(){
+    public void ingresarMateria(ArrayList<Materia> materias){
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("INGRESO DE MATERIA");
+        System.out.print("Ingrese el código de la materia: ");
+        String codigoIngresado = sc.nextLine();
+        System.out.print("\nIngrese el código de la materia: ");
+        String nombreIngresado = sc.nextLine();
+        System.out.print("\nIngrese la cantidad de niveles para las preguntas: ");
+        int numNivelesIngresado = sc.nextInt();
+        
+        sc.close();
+        
+        materias.add(new Materia(codigoIngresado,nombreIngresado,numNivelesIngresado));
     }
     
     //método que permite editar una materia
-    public void editarMateria(){
+    public void editarMateria(ArrayList<Materia> materias){
+        Scanner sc = new Scanner(System.in);
+        
+        int opcion = 0;
+        while (opcion != 3){
+            do {
+                System.out.println("MODIFICACIÓN DE MATERIA");
+                System.out.println("1. Modificar por código de la materia");
+                System.out.println("2. Modificar por nombre de la materia");
+                System.out.println("3. Regresar");
+                System.out.println("Ingrese una opcion: ");
+                opcion = sc.nextInt();
+                if (opcion < 1 || opcion > 3){
+                    System.out.println("ERROR: Opcion incorrecta");
+                }
+            } while (opcion < 1 || opcion > 3);
+            int i = 0;
+            int posicion = -1;
+            if (opcion == 1){
+                System.out.print("Ingrese el código de la materia a editar: ");
+                String codigoIngresado = sc.nextLine();
+                for (Materia m: materias){
+                    if (m.getCodigo().equals(codigoIngresado)){
+                        posicion = i;
+                    }
+                    i++;
+                }
+            } else if (opcion == 2){
+                System.out.print("Ingrese el código de la materia a editar: ");
+                String nombreIngresado = sc.nextLine();
+                for (Materia m: materias){
+                    if (m.getNombre().equals(nombreIngresado)){
+                        posicion = i;
+                    }
+                    i++;
+                }
+            }
+            if (posicion != -1 && (opcion == 1 || opcion == 2)){
+                int opcion2;
+                do{
+                    System.out.println("1. Modificar nombre de la materia");
+                    System.out.println("2. Modificar cantidad de niveles de la materia");
+                    opcion2 = sc.nextInt();
+                    if (opcion2 != 1 && opcion2 != 2){
+                        System.out.println("ERROR: Opcion incorrecta");
+                    }
+                }while(opcion2 != 1 && opcion2 != 2);
+                if (opcion2 == 1){
+                    System.out.println("Ingrese nuevo nombre de la materia: ");
+                    String nombreNuevo = sc.nextLine();
+                    materias.get(posicion).setNombre(nombreNuevo);
+                    
+                } else if (opcion2 == 2){
+                    System.out.println("Ingrese nueva cantidad de niveles de la materia: ");
+                    int numNivelesNuevo = sc.nextInt();
+                    materias.get(posicion).setNumeroNiveles(numNivelesNuevo);
+                }
+            }
+        }
+        
+        sc.close();
         
     }
     
-    //método que permite eliminar una materia
-    public void eliminarMateria(){
-        
+    @Override
+    public String toString(){
+        return "codigo: " + codigo + 
+                "; materia: " + nombre;
     }
-    
     
 }
--- Proyecto/src/main/java/com/mycompany/proyecto/Paralelo.java
+++ Proyecto/src/main/java/com/mycompany/proyecto/Paralelo.java
@@ -1,26 +1,33 @@
 package com.mycompany.proyecto;
 
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Paralelo {
-    private String numeroParalelo;
+    private int numeroParalelo;
     private Estudiante[] estudiantes;
     private TerminoAcademico termino;
     private Materia materia;
 
-    //constructor con todos los parámetros
-    public Paralelo(String numeroParalelo, Estudiante[] estudiantes, TerminoAcademico termino, Materia materia) {
+    public Paralelo(int numeroParalelo, TerminoAcademico termino, Materia materia) {
         this.numeroParalelo = numeroParalelo;
-        this.estudiantes = estudiantes;
         this.termino = termino;
         this.materia = materia;
     }
 
+    //constructor con todos los parámetros
+    public Paralelo(int numeroParalelo, Estudiante[] estudiantes, TerminoAcademico termino, Materia materia) {
+        this(numeroParalelo,termino,materia);
+        this.estudiantes = estudiantes;
+    }
+
     
-    public String getNumeroParalelo() {
+    public int getNumeroParalelo() {
         return numeroParalelo;
     }
 
-    public void setNumeroParalelo(String numeroParalelo) {
+    public void setNumeroParalelo(int numeroParalelo) {
         this.numeroParalelo = numeroParalelo;
     }
 
@@ -48,8 +55,82 @@ public class Paralelo {
         this.materia = materia;
     }
     
-    //método que permite editar el paralelo
-    public void editarParalelo(){
+    @Override
+    public String toString(){
+        return materia + "Termino: " + termino + "Paralelo: " + numeroParalelo;
+    }
+    
+    //método que permite agregar un paralelo a un término académico de una materia
+    public void agregarParalelo(ArrayList<Materia> materias, ArrayList<Paralelo> paralelos){
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("INGRESO DE PARALELO");
+        int i = 1;
+        //muestra una lista de todas las materias
+        for (Materia m: materias){
+            System.out.println(i + ". " + m);
+            i++;
+        }
+        //ingreso y validación de materia
+        int posicionMateria;
+        do {
+            System.out.print("Seleccione la materia: ");
+            posicionMateria = sc.nextInt();
+            if (posicionMateria < 1 || posicionMateria > materias.size()){
+                System.out.println("ERROR: Posición no existente");
+            }
+        }while (posicionMateria < 1 || posicionMateria > materias.size());
+        int j = 1;
+        //muestra una lista de todos los términos académicos de la materia seleccionada
+        for (TerminoAcademico t: materias.get(posicionMateria - 1).getTerminos()){
+            System.out.println("\t" + j + ". " + t);
+        }
+        //ingreso y validación de término académico
+        int posicionTerminoAcademico;
+        do{
+            System.out.print("Seleccione el término académico: ");
+            posicionTerminoAcademico = sc.nextInt();
+            if (posicionTerminoAcademico < 1 || posicionTerminoAcademico > materias.get(posicionMateria - 1).getTerminos().size()){
+                System.out.println("ERROR: Posición no existente");
+            }
+        }while (posicionTerminoAcademico < 1 || posicionTerminoAcademico > materias.get(posicionMateria - 1).getTerminos().size());
+        
+        //ingreso y agregación de número de paralelo
+        System.out.print("Ingrese el número de paralelo: ");
+        int numParaleloIngresado = sc.nextInt();
+        
+        paralelos.add(posicionTerminoAcademico - 1,new Paralelo(numParaleloIngresado,materias.get(posicionMateria - 1).getTerminos().get(posicionTerminoAcademico - 1),materias.get(posicionMateria - 1)));
+        
+        
+        System.out.println("Se ha añadido correctamento el paralelo");
+        
+        sc.close();
+    }
+    
+    //método que permite eliminar un paralelo
+    public void eliminarParalelo(ArrayList<Paralelo> paralelos){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("ELIMINACIÓN DE PARALELO");
+        
+        //muestra una lista de todos los paralelos
+        int i = 1;
+        for (Paralelo p: paralelos){
+            System.out.println(i + ". " + p);
+        }
+        //ingreso y validación de paralelo
+        int posicionParalelo;
+        do{
+            System.out.print("Seleccione un paralelo a eliminar: ");
+            posicionParalelo = sc.nextInt();
+            if (posicionParalelo < 0 || posicionParalelo > paralelos.size()){
+                System.out.println("ERROR: Posición no existente");
+            }
+        }while (posicionParalelo < 0 || posicionParalelo > paralelos.size());
+        
+        //elimina el paralelo seleccionado
+        paralelos.remove(posicionParalelo - 1);
+        
+        sc.close();
     }
 }
