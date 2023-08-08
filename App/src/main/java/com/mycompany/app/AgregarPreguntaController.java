1b4fac7d0e56b8c84731faf71624ae65620bd8d1
Diego Araujo <dienarau@espol.edu.ec>
2023-08-07 23:22:43 -0500

    Avance configuración preguntas

---
+++ App/src/main/java/com/mycompany/app/AgregarPreguntaController.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.app;
+
+import Model.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author arauj
+ */
+public class AgregarPreguntaController implements Initializable {
+
+
+    @FXML
+    private Label lblTitulo;
+    @FXML
+    private TextField textEnunciado;
+    @FXML
+    private TextField textNivel;
+    @FXML
+    private TextField textRespuestaPosible3;
+    @FXML
+    private ComboBox<Materia> cmbMaterias;
+    @FXML
+    private TextField textRespuestaCorrecta;
+    @FXML
+    private TextField textRespuestaPosible1;
+    @FXML
+    private TextField textRespuestaPosible2;
+    @FXML
+    private Button BtnCancelar;
+    @FXML
+    private Button BtnIngresar;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cmbMaterias.getItems().setAll(Configuracion.materias);
+    }    
+    
+    @FXML
+    private void GoToPreguntas(ActionEvent event) throws IOException {
+        App.setRoot("Preguntas");
+    }
+
+    @FXML
+    private void IngresarPregunta(ActionEvent event) throws IOException {
+        if (!BtnIngresar.getText().equals("Guardar Cambios")) {
+            Materia materia = cmbMaterias.getValue();
+            String enunciado = textEnunciado.getText();
+            int nivel = Integer.parseInt(textNivel.getText());
+            String correctaRespuesta = textRespuestaCorrecta.getText();
+            String posibleRespuesta1 = textRespuestaPosible1.getText();
+            String posibleRespuesta2 = textRespuestaPosible2.getText();
+            String posibleRespuesta3 = textRespuestaPosible3.getText();
+            Opcion[] opciones = {new Opcion(correctaRespuesta,Respuesta.CORRECTO), new Opcion(posibleRespuesta1,Respuesta.INCORRECTO), new Opcion(posibleRespuesta2,Respuesta.INCORRECTO), new Opcion(posibleRespuesta3,Respuesta.INCORRECTO)};
+
+            materia.AgregarPreguntas(new Pregunta(enunciado,nivel, opciones));
+        }
+
+        App.setRoot("Preguntas");
+    }
+
+}
--- App/src/main/java/com/mycompany/app/PreguntasController.java
+++ App/src/main/java/com/mycompany/app/PreguntasController.java
@@ -4,13 +4,20 @@
  */
 package com.mycompany.app;
 
+import Model.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -21,18 +28,58 @@ public class PreguntasController implements Initializable {
 
     @FXML
     private Button BtnVolver;
+    @FXML
+    private ComboBox<Materia> cmbMateria;
+    @FXML
+    private TableColumn<Pregunta, Integer> ColNivel;
+    @FXML
+    private TableColumn<Pregunta, String> ColEnunciado;
+    @FXML
+    private TableColumn<Opcion, String> ColOpcionCorrecta;
+    @FXML
+    private TableColumn<Opcion, String> ColOpcionPosible;
+    @FXML
+    private TableView<Pregunta> TvPreguntas;
+    @FXML
+    private Button BtnAgregar;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
+        cmbMateria.getItems().setAll(Configuracion.materias);
+        ColNivel.setCellValueFactory(new PropertyValueFactory<>("nivel"));
+        ColEnunciado.setCellValueFactory(new PropertyValueFactory<>("enunciado"));
+        ColOpcionCorrecta.setCellValueFactory(new PropertyValueFactory("texto"));
+        ColOpcionCorrecta.setCellValueFactory(new PropertyValueFactory("texto"));
+    }
+    
     @FXML
     private void GoToConfiguracion(ActionEvent event) throws IOException {
         App.setRoot("Configuracion");
     }
     
+    @FXML
+    private void AgregarPregunta(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("AgregarPregunta.fxml"));
+        AgregarPreguntaController ct = new AgregarPreguntaController();
+
+        loader.setController(ct);//se asigna el controlador
+        AnchorPane root = (AnchorPane) loader.load();//carga los objetos del fxml
+        
+        App.changeRoot(root);
+    }
+    
+    @FXML
+    private void AgregarMateria(ActionEvent event) throws IOException {
+       FXMLLoader loader = new FXMLLoader(App.class.getResource("AgregarMateria.fxml"));
+        AgregarMateriaController ct = new AgregarMateriaController();
+
+        loader.setController(ct);//se asigna el controlador
+        AnchorPane root = (AnchorPane) loader.load();//carga los objetos del fxml
+        
+        App.changeRoot(root);
+    }
+
 }
